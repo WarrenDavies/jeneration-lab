3f7df10df558a14b61f4a2b3d39ae886ac807f2b
Warren Davies <warren_davies81@hotmail.com>
2025-12-31 23:26:14 +0000

    implemented DataConnector and JenerationRecord

--- src/jenerationlab/core/runner.py
+++ src/jenerationlab/core/runner.py
@@ -4,6 +4,7 @@ import sys
 from pydantic import BaseModel
 
 from jenerationutils.benchmarker.benchmarker import Benchmarker
+from jenerationutils.jenerationrecord import registry as recorder_registry
 from jenerationlab.schemas.base import BaseSchema
 
 
@@ -19,23 +20,21 @@ class Runner():
         self.experiment = experiment
         self.storage_manager = storage_manager
         self.storage_manager.setup_experiment_folders(self.start_timestamp_str)
-        self.generation_metadata = BaseSchema(**{**experiment_config["experiment"], **experiment_config["generator"]})
+        self.GenerationRecordClass = recorder_registry.get_class(core_config["output_data_type"])
+        self.run_context = {**experiment_config["experiment"], **experiment_config["generator"]}
         self.ParamsSchema = self.get_params_schema()
-        self.storage_manager.data_connection.create_new_data_source(["test"])
-        # print(self.data_connection)
-        # sys.exit()
 
     
     def get_params_schema(self):
-        # This eventually to be emitted from the generator
-        # e.g., ParamsSchema = self.parent.generator.get_params_schema()
-
+        # This lives here temporarily, need to update and republish the generator
+        # to emit the params class it expects.
+        # e.g., this will simply changes to ParamsSchema = self.generator.get_params_schema()
         class ParamsSchema(BaseModel):
             dtype: str = ""
             seed: int = 0
             height: int = 0
             width: int = 0
-            inf_steps: int = 0
+            num_inference_steps: int = 0
             guidance_scale: float = 0
             enable_attention_slicing: bool = True
             scheduler: str = ""
@@ -50,5 +49,14 @@ class Runner():
             self.experiment.generator.config.update(inference_config)
             with Benchmarker() as benchmarker:
                 self.storage_manager.images = self.experiment.generator.run_pipeline()
-            print(benchmarker.execution_time)
+            params_metadata = self.ParamsSchema(**self.experiment.generator.config)
+            generation_metadata_record = self.GenerationRecordClass(
+                schema=BaseSchema,
+                generation_metadata = {
+                    **self.experiment_config["generator"], 
+                    "params": dict(params_metadata)
+                }
+            )
+            data_row = generation_metadata_record.create_data_row()
+            self.storage_manager.data_connection.append_data(data_row)
             self.storage_manager.save()
